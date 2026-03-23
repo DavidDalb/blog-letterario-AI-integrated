dc29789b2bb4d5c2ff7fb46bcbafc875e2150f09
Davide <dalbenziodavide01@gmail.com>
2026-03-23 11:18:21 +0100

    feat(dto): aggiungi campi password e email + G&S, nascondi password dalle risposte API

--- src/main/java/it/david/dto/UtenteDTO.java
+++ src/main/java/it/david/dto/UtenteDTO.java
@@ -2,11 +2,35 @@ package it.david.dto;
 
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UtenteDTO {
 
 	private Long id;
 	private String username;
+	private String email;
 	
+	// Permette di ricevere soltanto la password da registrazione/login.
+	// ma impedisce l'invio nei JSON di risposta.
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	private String password;
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public UtenteDTO() {}
 
 	public Long getId() {
