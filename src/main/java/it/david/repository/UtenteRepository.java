c5262cf95ed1b6204ba08f4212928d6e285b9937
Davide <dalbenziodavide01@gmail.com>
2026-03-23 12:26:33 +0100

    feat(repository): aggiungi metodo 'existsByEmailAndIdNot' per validazione email

--- src/main/java/it/david/repository/UtenteRepository.java
+++ src/main/java/it/david/repository/UtenteRepository.java
@@ -13,4 +13,7 @@ public interface UtenteRepository extends JpaRepository<Utente, Long>{
 	List<Utente> findByUsernameContainingIgnoreCase(String username);
 	
 	boolean existsByEmail(String email);
+	
+	// Verifica se l'email è già usata da altri utenti, IdNot esclude L'id stesso, altrimenti sarebbe true con se stesso.
+	boolean existsByEmailAndIdNot(String email, Long id);
 }
