334231ff63f937c39b2e16b1f803758f3704201a
Davide <dalbenziodavide01@gmail.com>
2026-04-14 16:29:21 +0200

    feat(swagger): implementa autenticazione tramite JWT per swagger

---
+++ src/main/java/it/david/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package it.david.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {          //Questa classe serve per informare Swagger l'uso di JWT
+
+    @Bean 
+     OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+            .components(new Components()
+                .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")));
+    }
+}
